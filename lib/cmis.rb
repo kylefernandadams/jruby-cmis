e1d31db4ff1d672e0428f63f350a1040ca0c7f82
Richard Nyström <ricny046@gmail.com>
2012-09-18 23:17:06 +0200

    Setting up the Java stuff in the lib

--- lib/cmis.rb
+++ lib/cmis.rb
@@ -1,5 +1,12 @@
-require "cmis/version"
+raise "You need to run JRuby to use CMIS" unless RUBY_PLATFORM =~ /java/
 
-module Cmis
+require "cmis/version"
+require 'java' 
+
+Dir[File.join(File.dirname(__FILE__), "../target/dependency/*.jar")].each do |jar|
+  require jar
+end
+
+module CMIS
   # Your code goes here...
 end
