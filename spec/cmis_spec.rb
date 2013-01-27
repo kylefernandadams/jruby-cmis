732c6857a88ca9ffb03bb7fe7f0f6c2ae3ba36ce
Richard Nyström <ricny046@gmail.com>
2013-01-27 21:29:52 +0100

    Much better specs that run against Alfresco and Nuxeo at the same time

--- spec/cmis_spec.rb
+++ spec/cmis_spec.rb
@@ -1,202 +1,124 @@
-# encoding: utf-8
-
 require 'spec_helper'
 
-describe CMIS do  
-  
-  describe "Running against a local OpenCMIS InMemory Repository" do
-
+describe "CMIS" do
+  shared_examples_for "a CMIS repository" do |atom_url, user, password|
     before(:all) do
-      @atom_url = "http://localhost:8080/chemistry-opencmis-server-inmemory-0.8.0/atom"
-      @user = "admin"
-      @password = "admin"
+      @atom_url = atom_url
+      @user = user
+      @password = password
       @repos = CMIS::repositories(@atom_url, @user, @password)
-      @session = CMIS::create_session(@atom_url, @user, @password, @repos[0].id) 
+      @session = CMIS::create_session(@atom_url, @user, @password, @repos[0].id)
+      @test_folder_name = "JRUBY_CMIS_TEST_" + random_name
+      @test_folder = @session.root_folder.create_cmis_folder(@test_folder_name)
     end
 
-    it "should create a session" do
-      @session.is_a?(Java::OrgApacheChemistryOpencmisClientRuntime::SessionImpl).should be_true
+    after(:all) do
+      @test_folder.delete_tree(true, CMIS::UnfileObject::DELETE, true)
     end
 
-    it "should retrieve all available repositories" do
-      @repos.is_a?(Java::JavaUtil::ArrayList).should be_true
-    end
-
-    it "should have one repository with a name" do
-      repo = @repos[0]
-      repo.name.should == "Apache Chemistry OpenCMIS InMemory Repository"
-    end
-
-    it "should be possible to find the contents of the root folder" do
-      root = @session.root_folder
-      children = root.children
-      children.map(&:name).should include("My_Document-0-0")
-    end
-
-    it "should create a folder object" do
-      random_name = rand(8**8).to_s(8)
-      new_folder_props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:folder", CMIS::PropertyIds::NAME => random_name }
-      @session.root_folder.create_folder(new_folder_props)
-      @session.root_folder.children.map(&:name).should include(random_name)
-    end
-
-    def content_as_string(stream)
-      sb = java.lang.StringBuilder.new
-      br = java.io.BufferedReader.new(java.io.InputStreamReader.new(stream.stream, "UTF-8"))
-      line = br.read_line
-      while line != nil do
-        line = br.read_line
-        sb.append(line)
+    describe "Repository functionality" do
+      it "should create a session" do
+        @session.is_a?(Java::OrgApacheChemistryOpencmisClientRuntime::SessionImpl).should be_true
       end
-
-      br.close
-      sb.to_s
-    end
-
-    def create_content_stream(filename)
-      content = nil
-
-      if filename != nil && filename.length > 0
-        file = java.io.File.new(filename)
-        stream = java.io.BufferedInputStream.new(java.io.FileInputStream.new(file))
-        content = @session.object_factory.create_content_stream(file.name, file.length, CMIS::MimeTypes.getMIMEType(file), stream)
-      end
-
-      content
-    end
-
-    # TODO: FIX THIS SHIT
-    it "should create a simple document object" do
-      content_stream = create_content_stream(file_path("text_file.txt"))
-      text_file = rand(8**8).to_s(8) + ".txt"
-      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document", CMIS::PropertyIds::NAME => text_file }
-      id = @session.root_folder.create_document(props, content_stream, CMIS::VersioningState::NONE)
-      
-      # Let's read the content from the server
-      doc = @session.get_object(id)
-      content_from_server = content_as_string(doc.content_stream)
-      
-      puts "content from server as string: " + content_from_server.to_s  
-      puts "content from server: " + doc.content_stream.to_s
-    end
-
-    it "should rename a document" do
-      content_stream = create_content_stream(file_path("text_file.txt"))
-      text_file = rand(8**8).to_s(8) + ".txt"
-      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document",
-                CMIS::PropertyIds::NAME => text_file }
-      id = @session.root_folder.create_document(props, content_stream, CMIS::VersioningState::NONE)
-
-      doc = @session.get_object(id)
-      renamed_text_file = "renamed_" + text_file
-      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document", 
-                CMIS::PropertyIds::NAME => renamed_text_file }
-      
-      doc.update_properties(props)
-
-      doc.name.should == renamed_text_file
-    end
-
-    it "should update the content of a document" do
-      pending "Load a freaking fixture!!"
-    end
-
-    it "should delete a document" do
-      text_file = rand(8**8).to_s(8) + ".txt"
-      mimetype = "text/plain; charset=UTF-8"
-      content = java.lang.String.new("This is some test content.")
-      buf = nil
-      buf = content.getBytes("UTF-8")
-      input = java.io.ByteArrayInputStream.new(buf)
-      content_stream = @session.object_factory.create_content_stream(text_file, buf.length, mimetype, input)
-      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document",
-                CMIS::PropertyIds::NAME => text_file }
-      id = @session.root_folder.create_document(props, content_stream, CMIS::VersioningState::NONE)
-
-      doc = @session.get_object(id)
-     
-      doc.delete(true)
-      children = @session.root_folder.children
-      children.map(&:name).should_not include(text_file)
-    end
-
-    it "should delete a folder tree" do
-      root = @session.root_folder
-      random_folder_name = rand(8**8).to_s(8)
-      folder1 = root.create_folder({ CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:folder", CMIS::PropertyIds::NAME => random_folder_name })
-      folder11 = folder1.create_folder({ CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:folder", CMIS::PropertyIds::NAME => "Folder11" })
-      folder12 = folder1.create_folder({ CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:folder", CMIS::PropertyIds::NAME => "Folder12" })
-      @session.root_folder.children.map(&:name).should include(random_folder_name)
-      folder1.delete_tree(true, CMIS::UnfileObject::DELETE, true)
-      @session.root_folder.children.map(&:name).should_not include(random_folder_name)
-    end
-
-    it "should be possible to navigate through a folder tree" do
-      root = @session.root_folder
-      # just making sure it can be executed
-      root.descendants(-1).count.should > 0 # get folders and documents
-      root.folder_tree(-1).count.should > 0 # get folders only
-    end
     
-    it "should display the properties of an object" do
-      props = @session.root_folder.properties   
-      props.each do |p|
-        disp_name = p.definition.display_name
-        p.value_as_string.should == "/" if disp_name == "Path"
-        p.value_as_string.should == "cmis:folder" if disp_name == "Type-Id"
-        p.value_as_string.should == "Admin" if disp_name == "Created By"
-        p.value_as_string.should == "RootFolder" if disp_name == "Name"
-        p.value_as_string.should == "100" if disp_name == "Object Id"
-        p.value_as_string.should be_nil if disp_name == "Parent Id"
+      it "should create a session against the first repository" do
+        @simple_session = CMIS::create_session(@atom_url, @user, @password)
+        @simple_session.is_a?(Java::OrgApacheChemistryOpencmisClientRuntime::SessionImpl).should be_true 
+      end
+
+      it "should retrieve all available repositories" do
+        @repos.is_a?(Java::JavaUtil::ArrayList).should be_true
+      end
+
+      it "should have one repository with a name" do
+        repo = @repos[0]
+        repo.name.should_not be_empty
+      end
+
+      it "should be possible to read the repository info" do
+        rep_info = @session.repository_info
+        cap = rep_info.capabilities
+
+        # Just testing a few of them
+        cap.is_get_descendants_supported.should == true
+        cap.is_get_folder_tree_supported.should == true
+        cap.changes_capability.value.should_not be_empty
+        cap.query_capability.value.should_not be_empty
+        cap.join_capability.value.should_not == be_empty
       end
     end
 
-    it "should be possible to get a property explicitly" do
-      # A couple of folder specific props that we can access explicitly
-      root = @session.root_folder
-      root.is_root_folder.should == true
-      root.path.should == "/"
-    end
+    describe "Reading objects" do
+      it "should be possible to find the contents of the root folder" do
+        root = @session.root_folder
+        children = root.children
+        children.map(&:name).should include(@test_folder_name)
+      end
 
-    it "should be possible to execute a simple query" do
-      query = "SELECT * FROM cmis:document WHERE cmis:name LIKE 'My_Document-0-0'"
-      q = @session.query(query, false)
-      q.count.should == 1
-      q.each do |result|
-        result.property_by_query_name("cmis:name").first_value.should == "My_Document-0-0"
-        result.property_by_query_name("cmis:objectTypeId").first_value.should == "ComplexType"
-        result.property_by_query_name("cmis:contentStreamLength").first_value.should == "33401"
+      it "should display the properties of an object" do
+        props = @session.root_folder.properties   
+        props.each do |p|
+          disp_name = p.definition.display_name
+          p.value_as_string.should == "/" if disp_name == "Path"
+          p.value_as_string.should == "cmis:folder" if disp_name == "Type-Id"
+          p.value_as_string.should_not be_nil if disp_name == "Name"
+          p.value_as_string.should be_nil if disp_name == "Parent Id"
+        end
+      end
+
+      it "should be possible to get a property explicitly" do
+        # A couple of folder specific props that we can access explicitly
+        root = @session.root_folder
+        root.is_root_folder.should == true
+        root.path.should == "/"
+      end
+
+      it "should retrieve allowable actions for an object" do
+        allowed_actions = @session.root_folder.allowable_actions.allowable_actions # Are you kidding me!!?
+        allowed_actions.to_a.should include(CMIS::Action::CAN_GET_PROPERTIES)
+      end
+
+      it "should be possible to check if a document is versionable" do
+        file = file_path("text_file.txt")
+        name = random_name + ".txt"
+        id = @test_folder.create_cmis_document(name, file)
+        doc = @session.get_object(id)
+        doc.type.is_versionable.should == true
+      end
+
+      it "should be possible to access information about renditions" do
+        file = file_path("document.pdf")
+        file_name = random_name + ".pdf"
+        id = @test_folder.create_cmis_document(file_name, file)      
+        context = @session.create_operation_context
+        context.rendition_filter_string = "cmis:thumbnail"
+        @session.clear
+        doc = @session.get_object(id, context)
+
+        renditions = doc.renditions
+        # TODO: Fix this test.
+        #renditions.size.should >= 1
+        renditions.each do |r|
+          r.kind.should_not be_empty
+          r.mime_type.should_not be_empty
+        end
       end
     end
+  end
 
-    it "should be possible to read the repository info" do
-      rep_info = @session.repository_info
-      cap = rep_info.capabilities
+  describe "Alfresco" do
+    atom_url = "http://cmis.alfresco.com/cmisatom"
+    user = "admin"
+    password = "admin"
+    
+    it_behaves_like "a CMIS repository", atom_url, user, password
+  end
 
-      # Just testing a few of them
-      cap.is_get_descendants_supported.should == true
-      cap.is_get_folder_tree_supported.should == true
-      cap.changes_capability.value.should == "none"
-      cap.query_capability.value.should == "bothcombined"
-      cap.join_capability.value.should == "none"
-    end
-
-    it "should throw a CmisBaseException when something goes wrong" do
-      pending
-    end
-
-    it "should retrieve allowable actions for an object" do
-      allowed_actions = @session.root_folder.allowable_actions.allowable_actions # Are you kidding me!!?
-      allowed_actions.to_a.should include(CMIS::Action::CAN_GET_PROPERTIES)
-    end
-
-    it "should be possible to check if a document is versionable" do
-      content_stream = create_content_stream(file_path("text_file.txt"))
-      text_file = rand(8**8).to_s(8) + ".txt"
-      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document", CMIS::PropertyIds::NAME => text_file }
-      id = @session.root_folder.create_document(props, content_stream, CMIS::VersioningState::NONE)
-      doc = @session.get_object(id)
-      doc.type.is_versionable.should == false
-    end
+  describe "Nuxeo" do
+    atom_url = "http://cmis.demo.nuxeo.org/nuxeo/atom/cmis"
+    user = "Administrator"
+    password = "Administrator"
+    
+    it_behaves_like "a CMIS repository", atom_url, user, password
   end
 end
