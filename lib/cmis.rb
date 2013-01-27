ca1f16de405668abb4627da76a7635f4467df30e
Richard Nyström <ricny046@gmail.com>
2013-01-27 02:46:01 +0100

    New specs running against Alfresco 4.2.c

--- lib/cmis.rb
+++ lib/cmis.rb
@@ -17,9 +17,33 @@ module CMIS
   import org.apache.chemistry.opencmis.commons.enums.VersioningState
   import org.apache.chemistry.opencmis.commons.enums.UnfileObject
   import org.apache.chemistry.opencmis.commons.impl.MimeTypes
+  import org.apache.chemistry.opencmis.commons.enums.Action
 
-  def self.create_session(url, user, password, repo_id)
+  FolderImpl = org.apache.chemistry.opencmis.client.runtime.FolderImpl
+  
+  class FolderImpl
+    def create_cmis_folder(name, props = nil)
+      folder_props = { PropertyIds::OBJECT_TYPE_ID => "cmis:folder", PropertyIds::NAME => name }
+      folder_props.merge!(props) if props != nil && props.is_a?(Hash)
+      self.create_folder(java.util.HashMap.new(folder_props))
+    end
+
+    def create_cmis_document(name, filename, props = nil)
+      if filename != nil && filename.length > 0
+        file = java.io.File.new(filename)
+        stream = java.io.BufferedInputStream.new(java.io.FileInputStream.new(file))
+        content = session.object_factory.create_content_stream(file.name, file.length, CMIS::MimeTypes.getMIMEType(file), stream)
+        doc_props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document", CMIS::PropertyIds::NAME => name }
+        doc_props.merge!(props) if props != nil && props.is_a?(Hash)
+        self.create_document(doc_props, content, CMIS::VersioningState::MAJOR)
+      end
+    end
+  end
+
+  def self.create_session(url, user, password, repo_id = nil)
       session_factory = SessionFactoryImpl.new_instance
+      repo_id = self.repositories(url, user, password)[0].id if repo_id == nil
+      
       parameters = { 
         SessionParameter::ATOMPUB_URL => url,
         SessionParameter::BINDING_TYPE => BindingType::ATOMPUB.value,
@@ -43,4 +67,29 @@ module CMIS
     session_factory.get_repositories(parameters)
   end
 
+  def self.create_content_stream(filename, session)
+    content = nil
+
+    if filename != nil && filename.length > 0
+      file = java.io.File.new(filename)
+      stream = java.io.BufferedInputStream.new(java.io.FileInputStream.new(file))
+      content = session.object_factory.create_content_stream(file.name, file.length, CMIS::MimeTypes.getMIMEType(file), stream)
+    end
+
+    content
+  end
+
+  def self.content_as_string(stream)
+    sb = java.lang.StringBuilder.new
+    br = java.io.BufferedReader.new(java.io.InputStreamReader.new(stream.stream, "UTF-8"))
+    line = br.read_line
+    
+    while line != nil do
+      line = br.read_line
+      sb.append(line)
+    end
+
+    br.close
+    sb.to_s
+  end
 end
---
+++ spec/alfresco_cmis_spec.rb
@@ -0,0 +1,190 @@
+require 'spec_helper'
+
+describe "Alfresco CMIS" do
+  
+  before(:all) do
+    @atom_url = "http://127.0.0.1:8181/alfresco/cmisatom"
+    @user = "admin"
+    @password = "admin"
+    @repos = CMIS::repositories(@atom_url, @user, @password)
+    @session = CMIS::create_session(@atom_url, @user, @password, @repos[0].id)
+  end
+
+  describe "Repository functionality" do
+    it "should create a session" do
+      @session.is_a?(Java::OrgApacheChemistryOpencmisClientRuntime::SessionImpl).should be_true
+    end
+
+    it "should create a session against the first repository" do
+      @simple_session = CMIS::create_session(@atom_url, @user, @password)
+      @simple_session.is_a?(Java::OrgApacheChemistryOpencmisClientRuntime::SessionImpl).should be_true 
+    end
+
+    it "should retrieve all available repositories" do
+      @repos.is_a?(Java::JavaUtil::ArrayList).should be_true
+    end
+
+    it "should have one repository with a name" do
+      repo = @repos[0]
+      repo.name.should == "Main Repository"
+    end
+
+    it "should be possible to read the repository info" do
+      rep_info = @session.repository_info
+      cap = rep_info.capabilities
+
+      # Just testing a few of them
+      cap.is_get_descendants_supported.should == true
+      cap.is_get_folder_tree_supported.should == true
+      cap.changes_capability.value.should == "none"
+      cap.query_capability.value.should == "bothcombined"
+      cap.join_capability.value.should == "none"
+    end
+  end
+
+  describe "Reading objects" do
+    it "should be possible to find the contents of the root folder" do
+      root = @session.root_folder
+      children = root.children
+      children.map(&:name).should include("Data Dictionary")
+    end
+
+    it "should display the properties of an object" do
+      props = @session.root_folder.properties   
+      props.each do |p|
+        disp_name = p.definition.display_name
+        p.value_as_string.should == "/" if disp_name == "Path"
+        p.value_as_string.should == "cmis:folder" if disp_name == "Type-Id"
+        p.value_as_string.should == "Admin" if disp_name == "Created By"
+        p.value_as_string.should == "Company Home" if disp_name == "Name"
+        p.value_as_string.should be_nil if disp_name == "Parent Id"
+      end
+    end
+
+    it "should be possible to get a property explicitly" do
+      # A couple of folder specific props that we can access explicitly
+      root = @session.root_folder
+      root.is_root_folder.should == true
+      root.path.should == "/"
+    end
+
+    it "should retrieve allowable actions for an object" do
+      allowed_actions = @session.root_folder.allowable_actions.allowable_actions # Are you kidding me!!?
+      allowed_actions.to_a.should include(CMIS::Action::CAN_GET_PROPERTIES)
+    end
+
+    it "should be possible to check if a document is versionable" do
+      content_stream = CMIS::create_content_stream(file_path("text_file.txt"), @session)
+      text_file = rand(8**8).to_s(8) + ".txt"
+      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document", CMIS::PropertyIds::NAME => text_file }
+      id = @session.root_folder.create_document(props, content_stream, CMIS::VersioningState::MAJOR)
+      doc = @session.get_object(id)
+      doc.type.is_versionable.should == true
+    end
+
+  end
+
+  describe "Writing objects" do
+    # TODO: before all: create a folder to put in the shit
+    # TODO: Clean up the mess afterwards
+
+    it "should create a folder" do
+      random_name = rand(8**8).to_s(8)
+      new_folder_props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:folder", CMIS::PropertyIds::NAME => random_name }
+      @session.root_folder.create_folder(new_folder_props)
+      @session.root_folder.children.map(&:name).should include(random_name)
+    end
+
+    it "should create a folder using convenient method" do
+      random_name = rand(8**8).to_s(8)
+      @session.root_folder.create_cmis_folder(random_name)
+      @session.root_folder.children.map(&:name).should include(random_name)
+    end
+
+    it "should create a folder using convenient method and specifying custom props"
+
+    it "should create document" do
+      content_stream = CMIS::create_content_stream(file_path("text_file.txt"), @session)
+      text_file = rand(8**8).to_s(8) + ".txt"
+      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document", CMIS::PropertyIds::NAME => text_file }
+      id = @session.root_folder.create_document(props, content_stream, CMIS::VersioningState::MAJOR)
+      doc = @session.get_object(id)
+      #puts doc.inspect # Do something here
+    end
+
+    it "should create document using convenient method" do
+      file = file_path("text_file.txt")
+      file_name = rand(8**8).to_s(8) + ".txt"
+      id = @session.root_folder.create_cmis_document(file_name, file)
+      doc = @session.get_object(id)
+      #puts doc.inspect # Do something here
+    end
+  end
+
+  describe "Updating objects" do
+    it "should rename a document" do
+      file = file_path("text_file.txt")
+      file_name = rand(8**8).to_s(8) + ".txt"
+      id = @session.root_folder.create_cmis_document(file_name, file)
+
+      doc = @session.get_object(id)
+      renamed_file_name = "renamed_" + file_name
+      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document", 
+                CMIS::PropertyIds::NAME => renamed_file_name }
+      doc.update_properties(props)
+
+      doc.name.should == renamed_file_name
+    end
+
+    it "should update the content of a document" do
+      pending "Load a freaking fixture!!"
+    end
+  end
+
+  describe "Deleting objects" do
+    it "should delete a document" do
+      content_stream = CMIS::create_content_stream(file_path("text_file.txt"), @session)
+      text_file = rand(8**8).to_s(8) + ".txt"
+      props = { CMIS::PropertyIds::OBJECT_TYPE_ID => "cmis:document",
+                CMIS::PropertyIds::NAME => text_file }
+      id = @session.root_folder.create_document(props, content_stream, CMIS::VersioningState::MAJOR)
+
+      doc = @session.get_object(id)
+     
+      doc.delete(true)
+      children = @session.root_folder.children
+      children.map(&:name).should_not include(text_file)
+    end
+
+    it "should delete a folder tree" do
+      root = @session.root_folder
+      random_folder_name = rand(8**8).to_s(8)
+      folder1 = root.create_cmis_folder(random_folder_name)
+      folder11 = folder1.create_cmis_folder("Folder11")
+      folder12 = folder1.create_cmis_folder("Folder12")
+      @session.root_folder.children.map(&:name).should include(random_folder_name)
+      folder1.delete_tree(true, CMIS::UnfileObject::DELETE, true)
+      @session.root_folder.children.map(&:name).should_not include(random_folder_name)
+    end
+
+  end
+
+  describe "Querying and navigating objects" do
+    it "should be possible to navigate through a folder tree" do
+      root = @session.root_folder
+      # just making sure it can be executed
+      root.descendants(-1).count.should > 0 # get folders and documents
+      root.folder_tree(-1).count.should > 0 # get folders only
+    end
+
+    it "should be possible to execute a simple query" do
+      query = "SELECT cmis:name FROM cmis:folder WHERE cmis:name = 'Company Home'"
+      q = @session.query(query, false)
+      
+      q.each do |result|
+        result.get_property_value_by_query_name("cmis:name").should == "Company Home"
+      end
+    end
+  end
+
+end
