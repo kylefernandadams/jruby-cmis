39f5f86e24f657a22a8671840dac953a6d74a2c8
Richard Nyström <ricny046@gmail.com>
2013-02-23 12:36:49 +0100

    Refactoring

--- lib/cmis/helpers.rb
+++ lib/cmis/helpers.rb
@@ -14,14 +14,11 @@ module CMIS
   end
 
   def self.create_content_stream(filename, session)
-    content = nil
-
-    if filename != nil && filename.length > 0
-      file = java.io.File.new(filename)
-      stream = java.io.BufferedInputStream.new(java.io.FileInputStream.new(file))
-      content = session.object_factory.create_content_stream(file.name, file.length, CMIS::MimeTypes.getMIMEType(file), stream)
-    end
-
+    file = java.io.File.new(filename)
+    file_input_stream = java.io.FileInputStream.new(file)
+    stream = java.io.BufferedInputStream.new(file_input_stream)
+    content = session.object_factory.create_content_stream(file.name, file.length, MimeTypes.getMIMEType(file), stream)
+    
     content
   end
 
