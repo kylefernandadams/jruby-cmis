ad1f18c72cbf247c5623689b34533d5252eb9781
Richard Nyström <ricny046@gmail.com>
2013-02-23 12:40:34 +0100

    Putting enums in a separate file.

--- lib/cmis/imports.rb
+++ lib/cmis/imports.rb
@@ -9,29 +9,4 @@ module CMIS
   import org.apache.chemistry.opencmis.commons.PropertyIds
   import org.apache.chemistry.opencmis.commons.impl.MimeTypes
   import org.apache.chemistry.opencmis.commons.data.AclCapabilities
-
-  import org.apache.chemistry.opencmis.commons.enums.Action
-  import org.apache.chemistry.opencmis.commons.enums.AclPropagation
-  import org.apache.chemistry.opencmis.commons.enums.BaseTypeId
-  import org.apache.chemistry.opencmis.commons.enums.CapabilityAcl
-  import org.apache.chemistry.opencmis.commons.enums.VersioningState
-  import org.apache.chemistry.opencmis.commons.enums.UnfileObject
-  import org.apache.chemistry.opencmis.commons.enums.BindingType
-  import org.apache.chemistry.opencmis.commons.enums.CapabilityChanges
-  import org.apache.chemistry.opencmis.commons.enums.CapabilityContentStreamUpdates
-  import org.apache.chemistry.opencmis.commons.enums.CapabilityJoin
-  import org.apache.chemistry.opencmis.commons.enums.CapabilityQuery
-  import org.apache.chemistry.opencmis.commons.enums.CapabilityRenditions
-  import org.apache.chemistry.opencmis.commons.enums.Cardinality
-  import org.apache.chemistry.opencmis.commons.enums.ChangeType
-  import org.apache.chemistry.opencmis.commons.enums.ContentStreamAllowed
-  import org.apache.chemistry.opencmis.commons.enums.DateTimeResolution
-  import org.apache.chemistry.opencmis.commons.enums.DecimalPrecision
-  import org.apache.chemistry.opencmis.commons.enums.ExtensionLevel
-  import org.apache.chemistry.opencmis.commons.enums.IncludeRelationships
-  import org.apache.chemistry.opencmis.commons.enums.PropertyType
-  import org.apache.chemistry.opencmis.commons.enums.RelationshipDirection
-  import org.apache.chemistry.opencmis.commons.enums.SupportedPermissions
-  import org.apache.chemistry.opencmis.commons.enums.UnfileObject
-  import org.apache.chemistry.opencmis.commons.enums.Updatability
 end
