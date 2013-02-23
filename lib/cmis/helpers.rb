6a49a58282389e2dfe7e691f8e697e5393b3f729
Richard Nyström <ricny046@gmail.com>
2013-02-23 12:30:33 +0100

    Refactoring

--- lib/cmis/helpers.rb
+++ lib/cmis/helpers.rb
@@ -1,28 +1,15 @@
 module CMIS
   def self.create_session(url, user, password, repo_id = nil)
     session_factory = SessionFactoryImpl.new_instance
+    params = session_params(url, user, password)
     repo_id = self.repositories(url, user, password)[0].id if repo_id == nil
-      
-    params = { 
-      SessionParameter::ATOMPUB_URL => url,
-      SessionParameter::BINDING_TYPE => BindingType::ATOMPUB.value,
-      SessionParameter::USER => user,
-      SessionParameter::PASSWORD => password,
-      SessionParameter::REPOSITORY_ID => repo_id
-    }
-
+    params[SessionParameter::REPOSITORY_ID] = repo_id
     session_factory.create_session(java.util.HashMap.new(params))
   end
 
   def self.repositories(url, user, password)
     session_factory = SessionFactoryImpl.new_instance
-      params = { 
-        SessionParameter::ATOMPUB_URL => url,
-        SessionParameter::BINDING_TYPE => BindingType::ATOMPUB.value,
-        SessionParameter::USER => user,
-        SessionParameter::PASSWORD => password
-      }
-
+    params = session_params(url, user, password)
     session_factory.get_repositories(java.util.HashMap.new(params))
   end
 
@@ -36,5 +23,18 @@ module CMIS
     end
 
     content
+  end
+
+  private
+  
+  def self.session_params(url, user, password)
+    params = { 
+      SessionParameter::ATOMPUB_URL => url,
+      SessionParameter::BINDING_TYPE => BindingType::ATOMPUB.value,
+      SessionParameter::USER => user,
+      SessionParameter::PASSWORD => password
+    }
+    
+    params
   end
 end
